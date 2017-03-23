e5f6fd866afcb8f80c2c34cff2b704c4c66b8276
Sishaar Rao <sishaar@gmail.com>
2017-03-23 15:31:31 -0400

    Successfully streams tweets from account @Sishaar

--- test.py
+++ test.py
@@ -2,16 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import tweepy, time, sys
+from secret import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+#argfile = str(sys.argv[1])
 
-argfile = str(sys.argv[1])
+try:
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    
+    client = tweepy.API(auth)
+    if not client.verify_credentials():
+        raise tweepy.TweepError
+    
+except tweepy.TweepError as e:
+    print('ERROR : connection failed. Check your OAuth keys.')
+else:
+    print('Connected as @{}, you can start to tweet !'.format(client.me().screen_name))
+    client_id = client.me().id
+    
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+#override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
 
-filename=open(argfile,'r')
-f=filename.readlines()
-filename.close()
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_data disconnects the stream
+            return False
+        
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = client.auth, listener=myStreamListener)
 
-for line in f:
-    api.update_status(line)
+followAcc = []
+followAcc.append(str((client.get_user("Sishaar")).id))
+print(followAcc)
+
+myStream.filter(follow=followAcc, async=True)
+#myStream.userstream(async=True)
+
+
+# For updating a status based on a textfile
+    
+#filename=open(argfile,'r')
+#f=filename.readlines()
+#filename.close()
+#for line in f:
+#    api.update_status(line)
