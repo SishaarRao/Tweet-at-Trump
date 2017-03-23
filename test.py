c429ba7fec5f3bd0f713320e5d850f6d7eec370f
Sishaar Rao <sishaar@gmail.com>
2017-03-23 11:58:31 -0400

    tweeting

--- test.py
+++ test.py
@@ -3,5 +3,15 @@
 
 import tweepy, time, sys
 
+argfile = str(sys.argv[1])
 
-print("Hello, World!")
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+filename=open(argfile,'r')
+f=filename.readlines()
+filename.close()
+
+for line in f:
+    api.update_status(line)
