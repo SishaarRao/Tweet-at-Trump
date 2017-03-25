22fcbad773c3935e7cfd34e956eca9b5916038dd
Sishaar Rao <sishaar@gmail.com>
2017-03-25 19:12:32 -0400

    finished setup, I probably should automate the last part

--- setup.sh
+++ setup.sh
@@ -5,7 +5,4 @@ git add --all
 git commit -m "Initial Commit"
 git push heroku master
 heroku ps:scale worker=1
-echo "Setup complete. Waiting for status ... "
-sleep 10
-heroku ps
-echo "If you see 'worker: crashed' refer README. Otherwise, Tweet@Trump is up!"
+echo "Setup complete. Run 'heroku ps' after some time. If you see 'worker: crashed' refer to README. Otherwise, Tweet@Trump is up!"
