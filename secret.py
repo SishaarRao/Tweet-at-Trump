64ee6d3d6cf9c4c00412edfd9609b0412fce19e3
Sishaar Rao <sishaar@gmail.com>
2017-03-25 18:24:05 -0400

    generic statement for keys

--- secret.py
+++ secret.py
@@ -1,9 +1,9 @@
 # Go to http://dev.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
-CONSUMER_KEY = 'u1sDSXcH0oExanIMHSSxFRHHO'
-CONSUMER_SECRET = 'RZjRxhyryLs5C0Qa4DFKiA7XlY8VvA7PfoKenvbZmUZHHFZuF3'
+CONSUMER_KEY = 'INSERT YOUR CONSUMER KEY HERE'
+CONSUMER_SECRET = 'INSERT YOUR SECRET CONSUMER KEY HERE'
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-ACCESS_KEY = '844934238856249345-cCd0W8RmMB1S3bk8SIq5Lt13UvKAfvt'
-ACCESS_SECRET = 'EKDQsuAJKwcKukmUJr5HgFnu6FkokJki9Ggkokx5HhRd0'
+ACCESS_KEY = 'INSERT YOUR ACCESS KEY HERE'
+ACCESS_SECRET = 'INSERT YOUR SECRET ACCESS KEY HERE'
