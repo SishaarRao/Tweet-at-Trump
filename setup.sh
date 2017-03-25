0817fc9332f88ea52ebdd0dcf46ae8cc0296ebea
Sishaar Rao <sishaar@gmail.com>
2017-03-25 19:08:12 -0400

    Initial Commit

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ git add --all
 git commit -m "Initial Commit"
 git push heroku master
 heroku ps:scale worker=1
-sleep 1
+sleep 5
 heroku ps
 echo "Setup complete. If you see 'worker: crashed' refer README"
