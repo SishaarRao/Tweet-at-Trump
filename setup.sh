c13b238bbb7b37fef0370d00f212f7eb51130b28
Sishaar Rao <sishaar@gmail.com>
2017-03-25 19:01:37 -0400

    first version of setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 heroku create
 git init
 git add --all
 git commit -m "Initial Commit"
+git push heroku master
+heroku ps:scale worker=1
+heroku ps
+echo "Setup complete. If you see 'worker: crashed' refer README"
